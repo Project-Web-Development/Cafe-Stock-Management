7519f3e3283cfa2136d47b8bb04378881c9d1a68
RayhanSipalingIt <116495289+RayhanSipalingIt@users.noreply.github.com>
2023-12-21 14:12:19 +0800

    sign in v2

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -2,12 +2,13 @@
 
 import { Card, Input, Button, Typography } from "@material-tailwind/react";
 import { useAuth } from "../Context/AuthContext";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../Configs/firebaseConfig";
 import { Navigate } from "react-router";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
+
 
 export default function SimpleRegistrationForm() {
   const { login } = useAuth();
@@ -15,6 +16,31 @@ export default function SimpleRegistrationForm() {
   const [password, setPassword] = useState("");
   const { isAuthenticated } = useAuth();
   const [erorAuth, setErorAuth] = useState(false);
+
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    // Add scroll event listener to hide/show scroll button based on scroll position
+    const scrollButton = document.getElementById('scrollButton');
+    const handleScroll = () => {
+      if (scrollButton) {
+        const isScrolled = window.scrollY > 0;
+        scrollButton.style.display = isScrolled ? 'none' : 'block';
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
 
   if (isAuthenticated()) {
     return <Navigate to={"/home"} />;
@@ -41,14 +67,24 @@ export default function SimpleRegistrationForm() {
   };
 
   return (
-    <div className="flex justify-between bg-gradient-to-b from-[#3F4E4F] to-[#2C3639]">
-      <div className="w-1/2 flex items-center justify-center h-screen ">
-        <h1 className="text-6xl font-bold text-[#DCD7C9] ms-10">
-          Manage and Control Your Cafe Stock
+    // <div className="flex flex-row md:flex-row bg-gradient-to-b from-[#3F4E4F] to-[#2C3639]">
+    <div className="flex flex-col md:flex-row items-center bg-gradient-to-b from-[#3F4E4F] to-[#2C3639]">
+  <div className="w-full flex h-screen items-center justify-center">
+    <h1 className="md:text-6xl text-4xl text-center font-bold text-[#DCD7C9]">
+      Manage and Control Your Cafe Stock
         </h1>
+        <div className="md:hidden block">
+        <Button
+          id="scrollButton"
+          className=" bg-blue-500 text-white px-6 py-3 rounded-full transition duration-300 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300 mt-4"
+          onClick={scrollToBottom}
+        >
+          Scroll to Bottom
+        </Button>
+        </div>
       </div>
-      <div className="w-1/2 flex items-center justify-center">
-        <div className="flex justify-center ps-5 pe-5 pb-5 rounded-3xl shadow-2xl bg-[#DCD7C9]">
+      <div className="w-full flex items-center justify-center">
+    <div className="flex justify-center ps-5 pe-9 pb-5 rounded-3xl shadow-2xl bg-[#DCD7C9] mt-10">
           <Stack sx={{ width: "100%", position: "relative" }}>
             <Card color="transparent" shadow={false} className="mx-auto mt-11">
               <Typography variant="h3" color="blue-gray">
@@ -123,5 +159,6 @@ export default function SimpleRegistrationForm() {
         </div>
       </div>
     </div>
+    // </div>
   );
 }
