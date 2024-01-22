83917a319d79f5d9bfb6ef29fc32540e64b3daf7
putrataufik <daengkadaeng@gmail.com>
2024-01-22 13:34:36 +0800

    Recipe Fix 3

--- src/Pages/InfoRecipe.jsx
+++ src/Pages/InfoRecipe.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import NavbarDefault from "../Components/NavigationBar";
 import { getMenuById } from "../Context/firebaseController";
 
-function InfoRecipe() {
-  const { id } = useParams();
-  const [menuData, setMenuData] = useState(null);
+function InfoRecipe({ idRecipe, setPopup }) {
+  const [menuData, setMenuData] = useState({});
+  
 
   useEffect(() => {
+    console.log(`idREcipe : ${idRecipe}`);
     const fetchMenuData = async () => {
       try {
-        const menuById = await getMenuById(id);
+        const menuById = await getMenuById(idRecipe);
+        console.log("WOOOI" + menuById);
         setMenuData(menuById);
         console.log("Data menu:", menuById.ingredients[0].ingredients);
       } catch (error) {
@@ -19,65 +20,60 @@ function InfoRecipe() {
     };
 
     fetchMenuData();
-  }, [id]);
+  }, []);
 
-  if (!menuData) {
-    return (
-      <div className="bg-gradient-to-b from-[#3F4E4F] to-[#2C3639] h-screen pt-2 flex flex-col">
-        <NavbarDefault />
-        <div className="text-white text-center mt-10">
-          <p>Data resep tidak ditemukan atau terjadi kesalahan.</p>
-        </div>
-      </div>
-    );
+  const handleExitButton = () =>{
+    setPopup(false)
   }
-
   return (
-    <div className="bg-gradient-to-b from-[#3F4E4F] to-[#2C3639] h-screen pt-2 flex flex-col">
-  <NavbarDefault />
+    <div className="h-screen w-screen absolute flex justify-center items-center z-10">
+      <div className="justify-center flex flex-col">
+        <div className="flex items-center bg-[#DCD7C9] pt-3 px-6 pb-8 rounded-lg max-w-3xl">
+          {/* Bagian kiri */}
+          <div>
+            <img
+              src={menuData.link}
+              alt={menuData.name}
+              className="border-b-[8px] border-indigo-500 shadow-lg w-64 object-cover rounded-3xl mb-2 h-[150px]"
+            />
+          </div>
 
-  <div className="flex items-center bg-[#DCD7C9] p-6 rounded-lg mx-auto mt-10 max-w-3xl">
-    {/* Bagian kiri */}
-    <div>
-      <img
-        src={menuData.link}
-        alt={menuData.name}
-        className="border-b-[8px] border-indigo-500 shadow-lg w-64 object-cover rounded-3xl mb-2 h-[150px]"
-      />
-    </div>
+          {/* Bagian kanan */}
+          <div className="ml-6">
+            <div className="flex justify-end">
 
-    {/* Bagian kanan */}
-    <div className="ml-6">
-      <div className="flex flex-col items-start">
-        <h3 className="text-2xl font-bold">{menuData.name}</h3>
-        <h5 className="text-md font-thin">{menuData.category}</h5>
-      </div>
-      <hr className="my-3 border-t border-black w-80" />
+          <button onClick={handleExitButton}>X</button>
+            </div>
+            <div className="flex flex-col items-start">
+              <h3 className="text-2xl font-bold">{menuData.name}</h3>
+              <h5 className="text-md font-thin">{menuData.category}</h5>
+            </div>
+            <hr className="my-3 border-t border-black w-80" />
 
-      <p className="text-lg font-extrabold">
-        Terjual: {menuData.sold} / Hari
-      </p>
+            <p className="text-lg font-extrabold">
+              Terjual: {menuData.sold} / Hari
+            </p>
 
-      {menuData.ingredients && menuData.ingredients.length > 0 && (
-        <div>
-          <p className="text-lg mt-2 font-bold">Bahan-bahan:</p>
-          <ul className="list-disc ml-6">
-            {menuData.ingredients.map((ingredient, index) => (
-              <li key={index} className="text-black">
-                {`${ingredient.quantity || ""} ${ingredient.unit || ""} ${
-                  ingredient.ingredients || ""
-                } (${ingredient.insideQuantityPerUnit || ""} ${
-                  ingredient.insideUnit || ""
-                })`}
-              </li>
-            ))}
-          </ul>
+            {menuData.ingredients && menuData.ingredients.length > 0 && (
+              <div>
+                <p className="text-lg mt-2 font-bold">Bahan-bahan:</p>
+                <ul className="list-disc ml-6">
+                  {menuData.ingredients.map((ingredient, index) => (
+                    <li key={index} className="text-black">
+                      {`${ingredient.quantity || ""} ${ingredient.unit || ""} ${
+                        ingredient.ingredients || ""
+                      } (${ingredient.insideQuantityPerUnit || ""} ${
+                        ingredient.insideUnit || ""
+                      })`}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
-      )}
+      </div>
     </div>
-  </div>
-</div>
-
   );
 }
 
